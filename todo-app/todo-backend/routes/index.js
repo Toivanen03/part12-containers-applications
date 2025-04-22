00fe12ae132bd18f32fced553c39be317be8dd02
Simo Toivanen <posti@simotoivanen.fi>
2025-04-22 15:43:45 +0300

    12.11 saakka palautukset

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const redis = require('../redis/index.js');
 const router = express.Router();
-
-const configs = require('../util/config')
+const { Todo } = require('../mongo')
+const configs = require('../util/config');
 
 let visits = 0
 
@@ -14,5 +15,26 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+router.post('/todos', async (req, res) => {
+  const todo = await Todo.create({
+    text: req.body.text,
+    done: req.body.done
+  });
+
+  if (todo) {
+    let count = await redis.getAsync('count') || 0;
+    count++;
+    await redis.setAsync('count', count);
+    res.send({ count });
+  } else {
+    res.status(400).send({ error: 'Todo creation failed' });
+  }
+});
+
+router.get('/statistics', async (req, res) => {
+  const todoCount = await redis.getAsync('count');
+  res.json({ added_todos: todoCount });
+})
 
 module.exports = router;
